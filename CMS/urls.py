59cb093c2e4649d4eb0aead723e67a093409ef51
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2020-06-23 23:11:33 +0530

    done

--- CMS/urls.py
+++ CMS/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
     
 ] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
