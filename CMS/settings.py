5894347f5c77358c14d49f831b8740a780fbb36a
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2020-06-24 22:45:45 +0530

    run test 3

--- CMS/settings.py
+++ CMS/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'CMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
     'default':  dj_database_url.config(
         default=config('DATABASE_URL')
